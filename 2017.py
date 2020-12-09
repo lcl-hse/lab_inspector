9ec1c601901efafe4919e26ac7db8e2eaf419508
Vantral <anton.buzanov.00@gmail.com>
2020-12-09 03:05:53 +0300

    Clear

--- 2017.py
+++
@@ -1,8 +0,0 @@
-import os
-import shutil
-
-for root, folders, files in os.walk('./exam/exam2017'):
-    for file in files:
-        if file.endswith('.txt'):
-            path = os.path.join(root, file)
-            shutil.move(path, './exam/exam2017')
